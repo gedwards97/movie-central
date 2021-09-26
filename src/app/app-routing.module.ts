3f5a20864e237e8e9990e4d0eba1620304e647bb
gedwards97 <georgeedwards97@hotmail.com>
2021-09-26 11:45:42 +0100

    Phase 1
    - Have got the foundations of the application working
    
    - Created initial 3 key components which include home, search and find movies. Home component is essentially complete for now,  find and search merely exist
    
    - Next phase will include setting out find and search pages, and initializing final result components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import application's components
+import { FindComponent } from './find/find.component';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'find', component: FindComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Custom components created for the application
+import { SearchComponent } from './search/search.component';
+import { FindComponent } from './find/find.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    FindComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
